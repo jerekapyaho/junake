033447888e094f5d9ad9dd8d14c5be5ad8989f33
Jere Käpyaho <Jere.Kapyaho@solita.fi>
2015-09-16 13:27:44 +0300

    Added parameters to live-trains.

--- app.py
+++ app.py
@@ -15,17 +15,24 @@ class Junake(object):
     @cherrypy.expose
     def stations(self):
         r = requests.get(BASE_URL + STATIONS_ENDPOINT)
+        
         return json.dumps(r.json(), indent=4)
 
     @cherrypy.expose
-    def live_trains(self, station=None):
+    def live_trains(self, station=None, arriving_trains=None, departing_trains=None):
         params = {}
         if station != None:
             params['station'] = station
-            
+        if arriving_trains != None:
+            params['arriving_trains'] = int(arriving_trains)
+        if departing_trains != None:
+            params['departing_trains'] = int(departing_trains)
+        print(params)
         r = requests.get(BASE_URL + LIVE_TRAINS_ENDPOINT, params)
+        print(r.url)
         return json.dumps(r.json(), indent=4)
         # Note that underscore in function name works out of the box for the dash in the URL
+        # (i.e., 'live-trains' maps to 'live_trains').
         
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0',})
